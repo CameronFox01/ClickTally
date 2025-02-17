1bcd37459091f1e074c27a3c8a7519e2bbc9f34d
Cameron Fox <cameron22401@icloud.com>
2025-02-16 20:58:33 -0700

    Initial Commit

---
+++ ClickTally/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClickTally
+//
+//  Created by Cameron Fox on 2/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
