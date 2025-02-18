62a41d3a6cbb2abc6aacb5d25c8c2ac4f1241610
Cameron Fox <158539819+CameronFox01@users.noreply.github.com>
2025-02-18 08:59:44 -0700

    This is the second attempt.
     Don't know what broke honestly

---
+++ ClickTally/ClickTally/ClickTallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickTallyApp.swift
+//  ClickTally
+//
+//  Created by Cameron Fox on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ClickTallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickTally/ClickTallyTests/ClickTallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickTallyTests.swift
+//  ClickTallyTests
+//
+//  Created by Cameron Fox on 2/18/25.
+//
+
+import Testing
+@testable import ClickTally
+
+struct ClickTallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
